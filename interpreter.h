5d1bd6d07d0538d0ccccb5f263490f206dadfa72
Aniketh Reddy <anikethjr@gmail.com>
2017-09-17 16:34:59 +0530

    Finished framework. Have to implement drawLine in Interpreter.h

--- interpreter.h
+++ interpreter.h
@@ -13,7 +13,8 @@ class Interpreter
         double leftangle;
         double rightangle;
         int length;
-        map<char,int> constants;
+        int radius;
+        map<char,pair<int,Color> > operations_map;
     public:
         /*
          * Constructor which initializes the grammar and the canvas used to draw the image
@@ -26,36 +27,84 @@ class Interpreter
          * @param rightangle The angle through which we must turn while branching right
          * @param length The length of the line segment to be drawn in the forward direction when the command to draw a line is given
          */
-        Interpreter(string axiom, string zerorulerhs, string onerulerhs, int width, int height, double leftangle, double rightangle, int length) : grammar(axiom,zerorulerhs,onerulerhs), canvas(width,height)
+        Interpreter(string axiom, string zerorulerhs, string onerulerhs, int width, int height, double leftangle, double rightangle, int length, int radius) : grammar(axiom,zerorulerhs,onerulerhs), canvas(width,height)
         {
             this->leftangle = leftangle;
             this->rightangle = rightangle;
             this->length = length;
+            this->radius = radius;
         }
         /**
          * Used to declare a character used in the grammar and define its meaning i.e. define the operation to be performed when the constant is encountered
          * @param character The character whose meaning is being defined
-         * @param operation The operation to be performed when the character is encountered. Can take any value from 1 to 5. 1 stands for draw forward, 2 stands for pushing current coordinates and inclination to stack, 3 stands for popping a set of coordinates and inclinations, 4 stands for turning left and 5 stands for turning right.
+         * @param operation The operation to be performed when the character is encountered. Can take any value from 1 to 5. 1 stands for draw forward, 2 stands for pushing current coordinates and inclination to stack, 3 stands for popping a set of coordinates and inclinations, 4 stands for turning left, 5 stands for turning right and 6 stands for drawing a circle.
          */
-        void addMeaning(char character, int operation)
+        void addMeaning(char character, int operation, Color* color = NULL)
         {
-            constants[character] = operation;
+            operations_map[character].first = operation;
+            if(color!=NULL)
+                operations_map[character].second = *color;
         }
         /**
          * Interprets the current string of the series and produces an interpretation i.e. image of it.
          */
+        void drawLine(int x, int y, double inclination, Color color)
+        {
+
+        }
         void interpret()
         {
             string current = grammar.getCurrent();
             long length = current.length();
-            int current_x = canvas.origin.first;
-            int current_y = canvas.origin.second;
+            pair<int,int> current_coordinates = canvas.getOrigin();
+            int current_x = current_coordinates.first;
+            int current_y = current_coordinates.second;
+            double inclination = 0;
+            stack<pair<int,int> > stack_coordinates;
+            stack<double> stack_inclinations;
             for (int i = 0; i < length; ++i)
             {
-                if(constants[current[i]]==1) //draw a forward line
+                /*
+                 * Draw a forward line at the current coordinates
+                 */
+                if(operations_map[current[i]].first==1)
+                    drawLine(current_x,current_y,inclination,operations_map[current[i]].second);
+                /*
+                 * Push current coordinates and inclination to stack
+                 */
+                else if(operations_map[current[i]].first==2)
                 {
-                    drawLine();
+                    stack_coordinates.push(pair<int,int> (current_x,current_y));
+                    stack_inclinations.push(inclination);
                 }
+                /*
+                 * Restore coordinates and inclination by popping from stack
+                 */
+                else if(operations_map[current[i]].first==3)
+                {
+                    current_x = stack_coordinates.top().first;
+                    current_y = stack_coordinates.top().second;
+                    inclination = stack_inclinations.top();
+                    stack_coordinates.pop();
+                    stack_inclinations.pop();
+                }
+                /*
+                 * Rotate left by specified number of degrees (leftangle)
+                 */
+                else if(operations_map[current[i]].first==4)
+                    inclination = inclination + leftangle;
+                /*
+                 * Rotate right by specified number of degrees (rightangle)
+                 */
+                else if(operations_map[current[i]].first==5)
+                    inclination = inclination - rightangle;
+                /*
+                 * Draw a circle at the current coordinates
+                 */
+                else if(operations_map[current[i]].first==6)
+                    canvas.drawCircle(current_x,current_y,radius,operations_map[current[i]].second);
+                else
+                    cout<<"Error 97: Unspecified constant found";
             }
         }
         /**
